6522efca4c0ccd5b45ef17dabb0542977d2ae8ca
LuizCorrea <lfscorr@gmail.com>
2024-09-10 18:08:44 -0300

    fundamentos003

---
+++ index.js
@@ -0,0 +1,14 @@
+let meta = {
+  value: 'ler um livro por mês',
+  checked: true
+}
+
+let metas = [
+  meta,
+  {
+    value: 'caminhar 20 minutos todos os dias',
+    cheked: false
+  }
+]
+
+console.log(metas[0].value)
